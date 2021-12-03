81185e6dc4b0fcb0d5a40eb6f99fb5442844c998
JhoanMontano1 <jhoanmontano19@gmail.com>
2021-12-03 16:37:38 -0600

    update

--- resources/views/componentes/header.blade.php
+++ resources/views/componentes/header.blade.php
@@ -5,8 +5,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>ABD SYSTEM</title>
-    <link href="{{asset('css/bootstrap.min.css')}}" rel="stylesheet">
-    <script src="{{asset('js/bootstrap.bundle.min.js')}}"></script>
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <script src="js/bootstrap.bundle.min.js"></script>
     <script src="{{asset('js/jquery.js')}}"></script>
     <script src="{{asset('js/moment.js')}}"></script>
     <link rel="stylesheet" href="{{asset('css/components.css')}}">  
