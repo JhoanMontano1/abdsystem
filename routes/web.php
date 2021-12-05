1955755214a9aad8ed299d5e206037f3de0e9f89
JhoanMontano1 <jhoanmontano19@gmail.com>
2021-12-04 20:34:29 -0600

    update

--- routes/web.php
+++ routes/web.php
@@ -71,4 +71,4 @@ Route::group(['middleware'=>'auth'],function(){
 
 });
 
-Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
